099520edc2d398f530cb0cfc00d4c7ab9f4967ce
Vasily Ulianov <Sorix@users.noreply.github.com>
2016-11-21 14:45:48 +0300

    Initial Commit

---
+++ uFreelance/ViewController.swift
@@ -0,0 +1,27 @@
+//
+//  ViewController.swift
+//  uFreelance
+//
+//  Created by Vasily Ulianov on 21.11.16.
+//  Copyright © 2016 Vasily Ulianov. All rights reserved.
+//
+
+import Cocoa
+
+class ViewController: NSViewController {
+
+	override func viewDidLoad() {
+		super.viewDidLoad()
+
+		// Do any additional setup after loading the view.
+	}
+
+	override var representedObject: Any? {
+		didSet {
+		// Update the view, if already loaded.
+		}
+	}
+
+
+}
+
