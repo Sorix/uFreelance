71870a77e2bc344dda3759d299c634e5519ae91d
Vasily Ulianov <Sorix@users.noreply.github.com>
2016-11-28 19:07:32 +0300

    Add Core Data

---
+++ uFreelance/RootSplitViewController.swift
@@ -0,0 +1,20 @@
+//
+//  RootSplitViewController.swift
+//  uFreelance
+//
+//  Created by Vasily Ulianov on 28.11.16.
+//  Copyright © 2016 Vasily Ulianov. All rights reserved.
+//
+
+import Cocoa
+
+class RootSplitViewController: NSSplitViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do view setup here.
+		
+		self.view.wantsLayer = true
+    }
+    
+}
